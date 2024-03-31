471dbbd196ad0cc82fa29613c0a2e1db68f5e2ed
root <nourisoukaina11@gmail.com>
2024-03-31 14:48:17 -0700

    js

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,21 +1,12 @@
 const assert = require('assert');
-const {it, describe} = require('mocha');
-const calculateNumber = require('./0-calcul');
+const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', () => {
-    it('checking if numbers round', () => {
-        assert.equal(calculateNumber(1, 3), 4);
-    });
-    it('checking if numbers round 2nd round', () => {
-        assert.equal(calculateNumber(1, 3.7), 5);
-    });
-    it('checking if numbers round 3nd round', () => {
-        assert.equal(calculateNumber(1.2, 3.7), 5);
-    });
-    it('checking if numbers round 4nd round', () => {
-        assert.equal(calculateNumber(1.5, 3.7), 6);
-    });
-    it('checking negative return', () => {
-        assert.equal(calculateNumber(-1.3, -3.7), -5);
-      });
+  it('should return -4 when inputs are -1 and -2.7', () => {
+    assert.strictEqual(calculateNumber(-1, -2.7), -4);
+  });
+
+  it('should return -4 when inputs are -1.7 and -2', () => {
+    assert.strictEqual(calculateNumber(-1.7, -2), -4);
+  });
 });
